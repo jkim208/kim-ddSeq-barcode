3be56df609e824f5ef23d773024cdfafb4f5ef11
Justin Kim <justinokim@gmail.com>
2017-09-22 13:07:46 -0400

    added edit distance checking and file writing feature. Read1 barcodes should now be properly added to read2 and saved in a new SAM file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # kim-ddSeq-barcode
 BioRad_ddSeq_Barcodes. Start date: 9/11/17
+
+To run script:
+
+$ python decode.py ddSeq.sam barcodeBlocks.txt 
+
+
--- decode.py
+++ decode.py
@@ -1,122 +1,123 @@
 #!/usr/bin/env python
-import re
-import editdistance # Find levenshtein distance
+import argparse  # command line options
+import regex  # regular expressions with edit distance functions
 import distance  # Find hamming distance
-from Bio import SeqIO
-from collections import Counter
 
 # Function "get_all_barcode_blocks" reads from a file containing all possible barcode blocks.
 # Returns a list with these barcode blocks.
-def get_all_barcode_blocks():
+def get_all_barcode_blocks(barcode_blocks_file):
     # read in all 96 possible cell barcode blocks in 'all_barcode_blocks'
-    with open('barcodeBlocks.txt', 'r') as barcode_blocks_file:
-        all_barcode_blocks = barcode_blocks_file.read().splitlines()
+    with open(barcode_blocks_file, 'r') as barcode_blocks_fh:
+        all_barcode_blocks = barcode_blocks_fh.read().splitlines()
         return all_barcode_blocks
-
-# Function "find_true_linkers" uses a small subset of random records to find the
-# true linker sequences.
-def find_true_linkers(sample_records):
-    linker1s = []
-    linker2s = []
-    for record in SeqIO.parse(sample_records, "fastq"):
-        seq_string = "%s" % record.seq
-        match_obj = re.match(r'.*([ACGT]{6})([ACGT]{15})([ACGT]{6})([ACGT]{15})([ACGT]{6})ACG([AGCT]{8})GACT',
-                             seq_string)
-        linker1s.append(match_obj.group(2))  # set of linker 1s
-        linker2s.append(match_obj.group(4))  # set of linker 2s
-
-    linker1_true = Counter(linker1s).most_common(1)[0][0]
-    linker2_true = Counter(linker2s).most_common(1)[0][0]
-    return linker1_true, linker2_true
 
 # Function "correct_bc_blocks" takes each barcode_block from a sequence, checks the hamming distances
 # between the variable block and all 96 possible blocks, and makes corrections depending on the smallest
 # ED(edit distance) found. Barcode block is unchanged if smallest ED is 2.
 def correct_bc_blocks(all_barcode_blocks, barcode_block):
     hamming_dict = {}
-    print('In function "correct_bc_blocks" for ' + barcode_block)
+
     # determine hamming distances between barcode block and all 96 possible blocks
     for i in range(95):
         hamming_dict[i] = (distance.hamming(barcode_block, all_barcode_blocks[i]))
 
     # find the blocks with the smallest hamming distances
     shd_index = min(hamming_dict, key=hamming_dict.get)
-    print('Smallest hamming dist:', hamming_dict[shd_index])  # value of smallest hamming distance
 
     if hamming_dict[shd_index] == 1:
         print('Fix barcode to the one which is 1 ED apart')
         barcode_block = all_barcode_blocks[shd_index]
-    print('Adjusted barcode is:' + all_barcode_blocks[shd_index])
+    elif hamming_dict[shd_index] == 2:
+        print('Smallest ED is 2. Barcode will be not be changed.')
+    elif hamming_dict[shd_index] > 2:
+        print('Smallest ED is > 2.')
+
+    # print('Adjusted barcode is:' + all_barcode_blocks[shd_index])
     return barcode_block
 
+# Function "demultiplex_barcodes" accounts for edit distance while extracting barcodes
+# Includes the correct_bc_blocks function in order to return full barcode
+def demultiplex_barcodes(all_records, all_barcode_blocks):
+    originalSAM = open(all_records, 'r')
+    barcodedRead2File = open('writeSamTest.sam', 'w')
 
-def demultiplex_barcodes(all_records, all_barcode_blocks, linker1_true, linker2_true):
-    linker1s = []  # define linker and barcode lists
-    linker2s = []
-    full_barcodes = []
-    full_barcodes_file = open('fullBarcodes.txt', 'w')  # store full barcodes in new file
-    # Loop through all records and follow decoding algorithm
-    # For linkers, allow only 1 edit distance
-    for record in SeqIO.parse(all_records, "fastq"):
-        print(record.id)
-        seq_string = "%s" % (record.seq)
-        match_obj = re.match(r'.*([ACGT]{6})([ACGT]{15})([ACGT]{6})([ACGT]{15})([ACGT]{6})ACG([AGCT]{8})GACT',
-                             seq_string)
-        linker1s.append(match_obj.group(2))
-        linker2s.append(match_obj.group(4))
-        if match_obj:
-            # print "match_obj.group() : ", match_obj.group()
-            # print "BC1       : ", match_obj.group(1)
-            # print("Linker1   : ", match_obj.group(2))
+    # write first two lines of sam file (header lines) to new file.
+    # start the loop once the pointer is on the actual records
+    barcodedRead2File.write(originalSAM.readline())
+    barcodedRead2File.write(originalSAM.readline())
+    # declare variable to hold one full barcode in one scope higher than the loop
+    # so that the barcode from read1 is 'saved' for appending to read2
+    bc_full_cell = ''
 
-            # evaluate edit distance. If >1 ED, skip this sequence
-            print('Edit distance for linker1: ', editdistance.eval(linker1_true, match_obj.group(2)))
-            if editdistance.eval(linker1_true, match_obj.group(2)) > 1:
-                print('linker 1 has >1 edit distance')
-                continue
-            # print "BC2       : ", match_obj.group(3)
-            # print("Linker2   : ", match_obj.group(4))
-            print('Edit distance for linker2: ', editdistance.eval(linker2_true, match_obj.group(4)))
-            if editdistance.eval(linker2_true, match_obj.group(4)) > 1:
-                print('linker 2 has >1 edit distance')
-                continue
-            # print "BC3       : ", match_obj.group(5)
-            # print "UMI       : ", match_obj.group(6)
-            bc1 = correct_bc_blocks(all_barcode_blocks, match_obj.group(1))
-            bc2 = correct_bc_blocks(all_barcode_blocks, match_obj.group(3))
-            bc3 = correct_bc_blocks(all_barcode_blocks, match_obj.group(5))
-            umi = match_obj.group(6)
-            print('UMI is ' + umi)
-            bc_full_cell = bc1 + bc2 + bc3 + umi
-            full_barcodes.append(bc_full_cell)
-            print('Full cell barcode is ' + bc_full_cell + '\n')
-            full_barcodes_file.write("%s\n" % bc_full_cell)
+    # loop through read1 records and apply decoding algorithm
+    for count, line in enumerate(originalSAM, start=0):
+        # every even line refers to read1. Decode read1 for barcodes. Do not write read1 to new SAM file
+        if count % 2 == 0:
+            match_obj1 = regex.match(r".*\t([NACGT]*)\t.*\t.*", line)
+            if match_obj1:
+                seq_string = match_obj1.group(1)
 
-        else:
-            print("No match")
+                # allow up to 1 edit distance(ED) when matching for Linkers
+                # ?e flag forces best matches (as few EDs as possible)
+                linker1 = regex.findall("(?e)(TAGCCATCGCATTGC){e<=1}", seq_string)
+                linker2 = regex.findall("(?e)(TACCTCTGAGCTGAA){e<=1}", seq_string)
 
-    full_barcodes_file.close()
-    return full_barcodes
+                if linker1 and linker2:
+                    # match blocks accordingly with linkers
+                    # consider potential insertions/deletions in barcodes/anchors
+                    # only keep reads where ACG and GACT anchors are not mutated
+                    # main unaccounted case is if insertions occur before/after barcode and before ACG
+                    regex_search = r".*([ACGT]{6})" + str(linker1[0]) + r"([ACGT]{6})" \
+                                   + str(linker2[0]) + r"([ACGT]{6})ACG([AGCT]{8}).?GACT"
+                    match_obj2 = regex.match(regex_search, seq_string)
 
+                    if match_obj2:
+                        bc1 = correct_bc_blocks(all_barcode_blocks, match_obj2.group(1))
+                        bc2 = correct_bc_blocks(all_barcode_blocks, match_obj2.group(2))
+                        bc3 = correct_bc_blocks(all_barcode_blocks, match_obj2.group(3))
+                        umi = match_obj2.group(4)
+                        bc_full_cell = bc1 + bc2 + bc3 + umi
+
+                    else:
+                        print("Regex match failed. Either sequence was mutated beyond acceptable measures"
+                              "or the wrong read was processed")
+
+                else:
+                    print("Linkers could not be found in good condition (ED > 1)")
+
+            else:
+                print('Did not match to a SAM record')
+
+        # every odd line is read2. Read2 will have its sequence appended by the previous read1 barcode
+        if count % 2 == 1:
+            read2 = line.split('\t')
+
+            # find where the sequence is in the list. Then, append the barcode
+            read2[9] = read2[9] + bc_full_cell
+
+            # rejoin the split line for writing to the new Sam file
+            barcodedRead2 = '\t'.join(read2)
+            barcodedRead2File.write(barcodedRead2)
+
+    originalSAM.close()
+    barcodedRead2File.close()
+    return
 
 def main():
+    # grab SAM filename/path from command line arguments
+    parser = argparse.ArgumentParser(description='Process paired reads from SAM file by extracting '
+                                                 'barcodes from read 1 and tagging them onto read 2')
+    parser.add_argument('filename1', help='(.sam file)')
+    parser.add_argument('filename2', help='(file containing barcode blocks)')
+    args = parser.parse_args()
+
     # obtain all possible barcode block combinations
-    all_barcode_blocks = get_all_barcode_blocks()
+    barcode_blocks_file = args.filename2
+    all_barcode_blocks = get_all_barcode_blocks(barcode_blocks_file)
 
-    # determine the true linker. Supply the function a subset of records in fastq format.
-    sample_records = "example.fastq"
-    (linker1_true, linker2_true) = find_true_linkers(sample_records)
-
-    # construct full cell barcodes from every sequence records. Supply the records in fastq format.
-    all_records = "example.fastq"
-    full_barcodes = demultiplex_barcodes(all_records, all_barcode_blocks, linker1_true, linker2_true)
-    # i = 0
-    # for record in SeqIO.parse("example2.fastq", "fastq"):
-    #     print(record.seq + full_barcodes[i])
-    #     i = i + 1
-    #     if i == len(full_barcodes):
-    #         break
-
+    # construct full cell barcodes from every sequence records. Supply the records in SAM format
+    sam_records_file = args.filename1
+    demultiplex_barcodes(sam_records_file, all_barcode_blocks)
 
 if __name__ == "__main__":
     main()
